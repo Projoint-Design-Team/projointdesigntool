d253ede89fbc2161df88d11c7e2f0b5e62821b1b
Akhat <akhatsuleimenov@gmail.com>
2024-07-12 13:34:25 +0400

    fix: docs display error

--- backend/documents/urls.py
+++ backend/documents/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import get_doc, list_docs
 
 urlpatterns = [
-    path("documents/list/", list_docs, name="list_docs"),
-    path("documents/<str:identifier>/", get_doc, name="get_doc"),
+    path("list/", list_docs, name="list_docs"),
+    path("<str:identifier>/", get_doc, name="get_doc"),
 ]
